388e30e7527cbb20039720a839c829bb7e07835a
Alexandre Talles <alexandretalles777@gmail.com>
2022-10-03 17:11:24 -0300

    wordInverter finished

--- app/src/main/java/com/example/multigames/WordInverter.kt
+++ app/src/main/java/com/example/multigames/WordInverter.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.core.widget.doOnTextChanged
 
 class WordInverter : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +14,26 @@ class WordInverter : AppCompatActivity() {
         setContentView(R.layout.activity_word_inverter)
 
         val btnBack = findViewById<Button>(R.id.btnBackWordInverter)
+        val btnInvert = findViewById<Button>(R.id.btnInvert)
+        val label = findViewById<TextView>(R.id.tvWordInverter)
+        val input = findViewById<EditText>(R.id.etWordInverter)
+
+
+        input.doOnTextChanged { text, start, before, count ->
+
+            label.text = ""
+
+        }
+
+        btnInvert.setOnClickListener {
+            val word = input.text
+            val invertedWord = word.reversed()
+
+            label.text = invertedWord
+
+
+        }
+
 
         btnBack.setOnClickListener {
             val intent = Intent(this, Menu::class.java)
